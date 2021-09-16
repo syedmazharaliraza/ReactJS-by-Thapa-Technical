8df70d90cbbd56a5d2e8ca2742f3fdada280fd56
Syed Mazhar Ali Raza <mazharali.raza11@gmail.com>
2021-09-16 23:41:16 +0530

    16septreact46

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App11 from "./App11";
-ReactDOM.render(<App11 />, document.getElementById("root"));
+import App12 from "./App12";
+ReactDOM.render(<App12 />, document.getElementById("root"));
