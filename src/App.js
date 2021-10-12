739d3b5706ac419698726d39bfb8aafe793a164d
Syed Mazhar Ali Raza <mazharali.raza11@gmail.com>
2021-10-12 21:30:42 +0530

    expense tracker project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ButtonAppBar from "./components/ButtonAppBar";
+import "./index.css";
+
+export default function App(){
+    return (
+        <>
+        <ButtonAppBar/>
+        </>
+    );
+};
